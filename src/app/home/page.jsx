89604775729e36e89a76007858254f26eeb46e63
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-04-14 20:36:03 +0600

    added the data featch related problem

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { IoMdAdd } from "react-icons/io";;
 import Link from "next/link";
-import { use } from "react";
-import fdData from "../../../public/fd.json"
+
 
 const page = async () => {
-
+    await new Promise(res => setTimeout(res, 200));
+    const res = await fetch("http://localhost:3000/fd.json", {
+        cache: "no-store"
+    })
+    const fdData = await res.json();
 
     const statusColor = {
         Overdue: "bg-red-300 rounded-2xl py-1 text-[15px] font-bold",
